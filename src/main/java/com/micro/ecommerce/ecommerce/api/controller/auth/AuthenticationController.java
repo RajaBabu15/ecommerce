2bda93a299662f201ab21610cd43402db7cde013
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2025-01-30 19:34:16 +0530

    add user registration functionality with validation and exception handling

--- src/main/java/com/micro/ecommerce/ecommerce/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/micro/ecommerce/ecommerce/api/controller/auth/AuthenticationController.java
@@ -1,4 +1,47 @@
 package com.micro.ecommerce.ecommerce.api.controller.auth;
 
+import com.micro.ecommerce.ecommerce.api.model.RegistrationBody;
+import com.micro.ecommerce.ecommerce.exception.UserAlreadyExistsException;
+import com.micro.ecommerce.ecommerce.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rest Controller for handling authentication requests.
+ */
+@RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
+
+    /** The user service. */
+    private final UserService userService;
+
+    /**
+     * Spring injected constructor.
+     * @param userService
+     */
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * Post Mapping to handle registering users.
+     * @param registrationBody The registration information.
+     * @return Response to front end.
+     */
+    @PostMapping("/register")
+    public ResponseEntity registerUser(@Valid @RequestBody RegistrationBody registrationBody) {
+        try {
+            userService.registerUser(registrationBody);
+            return ResponseEntity.ok("User registered successfully.");
+        } catch (UserAlreadyExistsException ex) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        }
+    }
+
 }
--- src/main/java/com/micro/ecommerce/ecommerce/service/UserService.java
+++ src/main/java/com/micro/ecommerce/ecommerce/service/UserService.java
@@ -1,4 +1,48 @@
 package com.micro.ecommerce.ecommerce.service;
 
+import com.micro.ecommerce.ecommerce.api.model.RegistrationBody;
+import com.micro.ecommerce.ecommerce.exception.UserAlreadyExistsException;
+import com.micro.ecommerce.ecommerce.model.LocalUser;
+import com.micro.ecommerce.ecommerce.model.dao.LocalUserDAO;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for handling user actions.
+ */
+@Service
 public class UserService {
+
+    /** The LocalUserDAO. */
+    private final LocalUserDAO localUserDAO;
+
+    /**
+     * Constructor injected by spring.
+     */
+    public UserService(LocalUserDAO localUserDAO) {
+        this.localUserDAO = localUserDAO;
+    }
+
+    /**
+     * Attempts to register a user given the information provided.
+     * @param registrationBody The registration information.
+     * @return The local user that has been written to the database.
+     * @throws UserAlreadyExistsException Thrown if there is already a user with the given information.
+     */
+    public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
+        if (localUserDAO.findByEmailIgnoreCase(registrationBody.getEmail()).isPresent()) {
+            throw new UserAlreadyExistsException("Email already exists.");
+        }
+        if (localUserDAO.findByUsernameIgnoreCase(registrationBody.getUsername()).isPresent()) {
+            throw new UserAlreadyExistsException("Username already exists.");
+        }
+        LocalUser user = new LocalUser();
+        user.setEmail(registrationBody.getEmail());
+        user.setUsername(registrationBody.getUsername());
+        user.setFirstName(registrationBody.getFirstName());
+        user.setLastName(registrationBody.getLastName());
+        //TODO: Encrypt passwords!!
+        user.setPassword(registrationBody.getPassword());
+        return localUserDAO.save(user);
+    }
+
 }
